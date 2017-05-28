45dc59a4b426ed078589f066f2ec4413a58831a6
londa101 <londa101@abv.bg>
2017-05-28 13:35:23 +0300

    add new changes

---
+++ Blog-master/Blog.UI.Tests/BrowserHost.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Chrome;
+
+using TestStack.Seleno.Configuration;
+
+namespace Blog.UI.Tests
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl = @"http://localhost:60634/Article/List";
+
+        static BrowserHost()
+        {
+          //  Instance.Run("Blog", 60639);
+            Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+
+            RootUrl = Instance.Application.Browser.Url;
+        }
+    }
+}
---
+++ Blog-master/Blog/Pages/Article/ArticlePage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace Blog.Pages.Article
+{
+    public partial class ArticlePage : BasePage
+    {
+        public ArticlePage(IWebDriver driver) : base(driver)
+        {
+        }
+        public void NavigateTo()
+        {
+            this.Driver.Navigate().GoToUrl("http://localhost:60634/Article/List");
+        }
+
+        public void FillArticle()
+        {
+
+        }
+    }
+}
